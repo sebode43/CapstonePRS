d4f29f79001c3e65d4177b5860e7c9ef42ebd70e
max-student <max-student@maxtrain.com>
2020-03-06 15:38:34 -0500

    Add project files.

---
+++ CapstonePRS/Data/CapstonePRSContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CapstonePRS.Models;
+
+namespace CapstonePRS.Data
+{
+    public class CapstonePRSContext : DbContext
+    {
+        public CapstonePRSContext (DbContextOptions<CapstonePRSContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder model) {
+            model.Entity<User>(e => {
+                e.ToTable("Users");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Username).HasMaxLength(30).IsRequired();
+                e.HasIndex(x => x.Username).IsUnique();
+                e.Property(x => x.Password).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Firstname).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Lastname).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Phone).HasMaxLength(12);
+                e.Property(x => x.Email).HasMaxLength(255);
+            });
+            model.Entity<Vendor>(e => {
+                e.ToTable("Vendors");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Code).HasMaxLength(30).IsRequired();
+                e.HasIndex(x => x.Code).IsUnique();
+                e.Property(x => x.Name).HasMaxLength(30).IsRequired();
+                e.Property(x => x.Address).HasMaxLength(30).IsRequired();
+                e.Property(x => x.City).HasMaxLength(30).IsRequired();
+                e.Property(x => x.State).HasMaxLength(2).IsRequired();
+                e.Property(x => x.Zip).HasMaxLength(5).IsRequired();
+                e.Property(x => x.Phone).HasMaxLength(12);
+                e.Property(x => x.Email).HasMaxLength(255);
+            });
+           
+        }
+
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+    }
+}
---
+++ CapstonePRS/Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CapstonePRS.Models {
+    public class User {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public bool IsReviewer { get; set; }
+        public bool IsAdmin { get; set; }
+
+        public User() { }
+    }
+}
